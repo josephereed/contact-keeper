1eda19fa4c19a11c5a9a4b236645e07dcf407b35
Joseph Reed <jereed559@gmail.com>
2020-04-30 18:51:30 -0700

    Removed initial route

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to the ContactKeeper API~~" });
-});
-
 // Define Routes
 app.use("/api/users", UsersRouter);
 app.use("/api/auth", AuthRouter);
